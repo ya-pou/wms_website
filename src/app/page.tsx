ed0e7f94e64fafd3b3c9e6bc2a08fb33409b8939
WebMate Services <haag.a1994@gmail.com>
2025-11-17 09:50:45 +0100

    change: Create layout folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layouts/Navbar";
 import Image from "next/image";
 import Button from "./components/ui/Button";
 import Services from "./components/sections/Services";
